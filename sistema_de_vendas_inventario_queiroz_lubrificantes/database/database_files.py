74794f1ca6d0fbb442d080f9e7903b168936de59
Diegiwg <diegiwg@gmail.com>
2023-04-17 23:48:03 -0300

    Modulo Database - v1.7.0
    Agora utiliza 'os.path.expanduser', para sempre criar a pasta Data dentro
    da pasta de usuario atual.
    
    Foi aplicada a mesma função nos demais caminhos.

--- sistema_de_vendas_inventario_queiroz_lubrificantes/database/database_files.py
+++ sistema_de_vendas_inventario_queiroz_lubrificantes/database/database_files.py
@@ -1,23 +1,23 @@
-from os import mkdir, path
+import os
 from typing import Literal
 
 from tinydb import TinyDB
 from tinydb.storages import MemoryStorage
 
 # Criar diretorio caso não exista
-if not path.exists("data"):
-    mkdir("data")
+if not os.path.exists(os.path.expanduser("~/datas")):
+    os.mkdir(os.path.expanduser("~/datas"))
 
 
 def produtos_db(storage: Literal["Memory", None] = None):
     if storage is None:
-        return TinyDB("data/produtos.json")
+        return TinyDB(os.path.expanduser("~/data/produtos.json"))
 
     return TinyDB(storage=MemoryStorage)
 
 
 def vendas_db(storage: Literal["Memory", None] = None):
     if storage is None:
-        return TinyDB("data/vendas.json")
+        return TinyDB(os.path.expanduser("~/data/vendas.json"))
 
     return TinyDB(storage=MemoryStorage)
