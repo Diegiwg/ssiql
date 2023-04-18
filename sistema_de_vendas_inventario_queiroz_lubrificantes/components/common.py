709140af681b6c4decf88c4ff5beaaf5eaae5d7a
Diegiwg <diegiwg@gmail.com>
2023-04-17 23:09:00 -0300

    Modulo Components - v1.3.0
    Aplicada formatação do Black.
    
    Criado o novo componente 'MultilineTextInput'.
    
    Adicionado ao componente 'Selection', a opção de configurar o evento
    'on_select'.
    
    Adicionado ao componente 'Divider', a opção de configurar o estilo, com
    uma configuração padrão de padding_top=10 e padding_bottom=10.
    
    Além do estilo, foi adicionado tambem ao 'Divider', a opção de configurar
    se ele sera na vertical ou horizontal (direction).

--- sistema_de_vendas_inventario_queiroz_lubrificantes/components/common.py
+++ sistema_de_vendas_inventario_queiroz_lubrificantes/components/common.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Callable, Literal, Optional, Union
 
 import toga
 
@@ -6,65 +6,90 @@ from .style import Style, StyleDocument
 
 
 class Box(toga.Box):
-    def __init__(self, children: list[toga.Widget] = [],
-                 style: StyleDocument = StyleDocument()):
-
+    def __init__(
+        self, children: list[toga.Widget] = [], style: StyleDocument = StyleDocument()
+    ):
         super().__init__(children=children)
         Style(self, style)
 
 
 class Column(Box):
-    def __init__(self, children: list[toga.Widget] = [],
-                 style: StyleDocument = StyleDocument()):
-
-        style['direction'] = 'column'
+    def __init__(
+        self, children: list[toga.Widget] = [], style: StyleDocument = StyleDocument()
+    ):
+        style["direction"] = "column"
         super().__init__(children=children, style=style)
 
 
 class Row(Box):
-    def __init__(self, children: list[toga.Widget] = [],
-                 style: StyleDocument = StyleDocument()):
-
-        style['direction'] = 'row'
+    def __init__(
+        self, children: list[toga.Widget] = [], style: StyleDocument = StyleDocument()
+    ):
+        style["direction"] = "row"
         super().__init__(children=children, style=style)
 
 
 class Divider(toga.Divider):
-    def __init__(self):
-        super().__init__()
-        Style(self)
+    def __init__(
+        self,
+        direction: Literal["h", "v"] = "h",
+        style: StyleDocument = StyleDocument(padding_top=10, padding_bottom=10),
+    ):
+        m_direction = 0 if direction == "h" else 1
+        super().__init__(direction=m_direction)
+        Style(self, style)
 
 
 class Label(toga.Label):
-    def __init__(self, text: str,
-                 style: StyleDocument = StyleDocument()):
+    def __init__(self, text: str, style: StyleDocument = StyleDocument()):
         super().__init__(text=text)
         Style(self, style)
 
 
 class NumberInput(toga.NumberInput):
-    def __init__(self, value: Union[int, None] = None,
-                 style: StyleDocument = StyleDocument()):
+    def __init__(
+        self, value: Union[int, None] = None, style: StyleDocument = StyleDocument()
+    ):
         super().__init__(value=value)
         Style(self, style)
 
 
 class TextInput(toga.TextInput):
-    def __init__(self, value: Union[str, None] = None, on_change=None,
-                 style: StyleDocument = StyleDocument()):
+    def __init__(
+        self,
+        value: Union[str, None] = None,
+        on_change=None,
+        style: StyleDocument = StyleDocument(),
+    ):
         super().__init__(value=value, on_change=on_change)
         Style(self, style)
 
 
 class Button(toga.Button):
-    def __init__(self, text: str, on_press=None,
-                 style: StyleDocument = StyleDocument()):
+    def __init__(
+        self, text: str, on_press=None, style: StyleDocument = StyleDocument()
+    ):
         super().__init__(text=text, on_press=on_press)
         Style(self, style)
 
 
 class Selection(toga.Selection):
-    def __init__(self, items: list[str] = [],
-                 style: StyleDocument = StyleDocument()):
-        super().__init__(items=items)
+    def __init__(
+        self,
+        items: list[str] = [],
+        style: StyleDocument = StyleDocument(),
+        on_select: Optional[Callable] = None,
+    ):
+        super().__init__(items=items, on_select=on_select)
+        Style(self, style)
+
+
+class MultilineTextInput(toga.MultilineTextInput):
+    def __init__(
+        self,
+        value: Optional[str] = None,
+        on_change=None,
+        style: StyleDocument = StyleDocument(),
+    ):
+        super().__init__(value=value, on_change=on_change)
         Style(self, style)
