673a2bd59ec3a096c4789d7994e843f167b991d3
Diegiwg <diegiwg@gmail.com>
2023-04-17 23:19:26 -0300

    Modulo Components - v1.5.0
    Aplicada formatação do Black.
    
    Removidos os argumetos 'model' e 'database', por não mais satisfazerem
    os requesitos de flexibilidade para a tabela.
    
    Foram adicionados os argumetos 'data_source' e 'filter_source', agora,
    responsaveis por gerenciar os dados da tabela.
    
    * Algumas modificações menores relacionadas a essa troca foram feitas.
    
    Em 'selected', foi adicionado um bloco try...except para evitar erros
    de execução quando a 'row' selecionada não possuir os atributos '_attrs'
    e '_source'.
    
    Adicionado o metodo 'table', para ter acesso ao widget interno da *tabela.
    
    Adicionado o metodo 'set_selected', para ser possivel manualmente
    selecionar substituir a informação da 'linha selecionada'.

--- sistema_de_vendas_inventario_queiroz_lubrificantes/components/table.py
+++ sistema_de_vendas_inventario_queiroz_lubrificantes/components/table.py
@@ -2,64 +2,69 @@
 Ainda precisa refatorar antes de lançar o Modulo
 """
 
-from typing import Union
+from typing import Callable
 
 import toga
 
-from ..database.database_api import ProdutoAPI, VendaAPI
 from .common import Box, Column, Label, Row, TextInput
 from .style import Style, StyleDocument
 
 
 class Table(Box):
-    def __init__(self,
-                 headings: list,
-                 model,
-                 database: Union[VendaAPI, ProdutoAPI],
-                 title='',
-                 style=StyleDocument()):
-
+    def __init__(
+        self,
+        headings: list,
+        data_source: Callable,
+        filter_source: Callable,
+        title="",
+        style=StyleDocument(),
+    ):
         super().__init__(style=style)
 
         self.__selected = None
-        self.__model = model
-        self.__database = database
+        self.__data_source = data_source
+        self.__filter_source = filter_source
 
         self.__table = toga.Table(
-            headings=headings, data=database.listar(),
-            missing_value="", on_select=self.__on_select_handler
+            headings=headings,
+            data=data_source(),
+            missing_value="",
+            on_select=self.__on_select_handler,
         )
-        Style(self.__table, {'flex': 1})
+        Style(self.__table, {"flex": 1})
 
         self.__search_input = TextInput(
-            value='',
-            on_change=self.__on_search_handler,
-            style={'flex': 1}
+            value="", on_change=self.__on_search_handler, style={"flex": 1}
         )
 
         self.add(
-            Column(style={'flex': 1}, children=[
-                Row(children=[
-                    Label(style={'flex': 3}, text=title),
-                    Row(style={'flex': 1}, children=[
-                        Label('Buscar'),
-                        self.__search_input
-                    ])
-                ]),
-                self.__table
-            ])
+            Column(
+                style={"flex": 1},
+                children=[
+                    Row(
+                        children=[
+                            Label(style={"flex": 3}, text=title),
+                            Row(
+                                style={"flex": 1},
+                                children=[Label("Buscar"), self.__search_input],
+                            ),
+                        ]
+                    ),
+                    self.__table,
+                ],
+            )
         )
 
     def __on_select_handler(self, table, row):
         self.__selected = row
 
     def __on_search_handler(self, _):
-        if self.__search_input.value == '':
-            self.__table.data = self.__database.listar()
+        if self.__search_input.value == "":
+            self.__table.data = self.__data_source()
             return
 
-        m_search = self.__search_input.value.lower()
-        self.__table.data = self.__database.buscar(m_search)
+        m_search: str = self.__search_input.value.lower()
+        self.__table.data = self.__filter_source(m_search)
 
     def update(self):
         self.__selected = None
@@ -74,7 +79,16 @@ class Table(Box):
 
         m_item: dict = self.__selected.__dict__
 
-        del m_item['_attrs']
-        del m_item['_source']
+        try:
+            del m_item["_attrs"]
+            del m_item["_source"]
+        except:  # noqa: E722
+            pass  # Não tem atributos
 
-        return self.__model(**m_item)
+        return m_item
+
+    def table(self):
+        return self.__table
+
+    def set_selected(self, row):
+        self.__selected = row
